b143b3c3d4b3b455fdb7258e4689e71f6950034b
Bryan99H <bryan99cente@gmail.com>
2023-04-01 12:09:32 -0600

    activity login

---
+++ app/src/main/java/cr/una/alpha/uniactivos/MainActivity.kt
@@ -0,0 +1,17 @@
+package cr.una.alpha.uniactivos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun siguiente(view: View){
+        val sig=  Intent(this, PasswordRecovery::class.java)
+        startActivity(sig)
+    }
+}
---
+++ app/src/main/java/cr/una/alpha/uniactivos/PasswordRecovery.kt
@@ -0,0 +1,11 @@
+package cr.una.alpha.uniactivos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordRecovery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_recovery)
+    }
+}
